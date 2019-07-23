f62f30a04d8d4f698bcf812f5f21ea0ac9177d43
Matt Wood <matt9663@gmail.com>
2019-07-23 10:40:51 -0400

    second wave of changes

---
+++ datastore.js
@@ -0,0 +1,104 @@
+const questionsData = [
+    {
+    question: `The four members of the Beatles were born in Liverpool, England and started playing music together as teenagers in the late 1950s. What was the initial name of the group before they rebranded themselves as the Beatles in 1960?`,
+    a: `The Birds`,
+    b: `The Quarrymen`,
+    c: `The Crickets`,
+    d: `The Hurricanes`,
+    answer: `The Quarrymen` 
+    },
+    {
+    question: `In what German city did the Fab Four spend nearly 2 years (1960-1962) honing their musical and performance chops in bars and nightclubs prior to releasing their debut album in 1963?`,
+    a: `Hamburg`,
+    b: `Munich`,
+    c: `Berlin`,
+    d: `Cologne`,
+    answer: `Hamburg`
+    },
+    {
+    question: `Who was the English record producer, often dubbed the “Fifth Beatle”, who worked with the band on all of their studio recordings?`,
+    a: `Bert Kampfert`,
+    b: `Berry Gordy`,
+    c: `Kanye West`,
+    d: `George Martin`,
+    answer: `George Martin`     
+    },
+    {
+    question: `The Beatles’ 1964 appearance on what US television show was watched by 34% of the American populace and launched the “British Invasion”?`,
+    a: `The Ed Sullivan Show`,
+    b: `The Andy Williams Show`,
+    c: `The Tonight Show with Johnny Carson`,
+    d: `American Bandstand`,
+    answer: `The Ed Sullivan Show`
+    },
+    {
+    question: `Which American musician reportedly introduced the Beatles to cannabis in 1964, thus kicking off their “psychedelic” years?`,
+    a: `Elvis Presley`,
+    b: `Bob Dylan`,
+    c: `Jimi Hendrix`,
+    d: `Jerry Lee Lewis`,
+    answer: `Bob Dylan`
+    },
+    {
+    question: `The Beatles set a record in 1964 by posting 5 simultaneous singles to the Billboard Top 10 charts. This record stood for 54 years until it was beaten in 2018 by which artist, who posted 7?`,
+    a: `Drake`,
+    b: `Adele`,
+    c: `Ariana Grande`,
+    d: `Katy Perry`,
+    answer: `Drake`
+    },
+    {
+    question: `Fed up by the grueling schedule and having their music drowned out by screaming fans, the Beatles decided to quit touring midway through their career. The band played their last concert for a paying audience on August 29th, 1966 at Candlestick Park in which US city?`,
+    a: `New York`,
+    b: `Dallas`,
+    c: `Chicago`,
+    d: `San Francisco`,
+    answer: `San Francisco`
+    },
+    {
+    question: `Sgt. Pepper’s Lonely Hearts Club Band was notable for being one of the first major rock records to include what in the linear notes of the album?`,
+    a: `Studio musician credits`,
+    b: `Artist commentary on the songs`,
+    c: `List of equipment used during the recording process`,
+    d: `Song lyrics`,
+    answer: `Song lyrics`
+    },
+    {
+    question: `Who is the only non-Beatle to be officially credited as a performer on a Beatles record, for his electric piano contributions on Get Back?`,
+    a: `Eric Clapton`,
+    b: `Andy White`,
+    c: `Billy Preston`,
+    d: `Pete Best`,
+    answer: `Billy Preston`
+    },
+    {
+    question: `Due to rampant internal tensions, the Beatles disbanded before completing their last album Let It Be. Who was the record producer that was tapped to finish the album?`,
+    a: `Brian Wilson`,
+    b: `Phil Spector`,
+    c: `Willie Mitchell`,
+    d: `Benny Andersson`,
+    answer: `Phil Spector`
+    }
+];
+const iconData = [
+    {
+    imgURL: `https://user-images.githubusercontent.com/51541006/61715438-af333800-ad4c-11e9-9a59-7aa6d1353e19.jpg`,
+    alt: `crying paul`
+    },
+    {
+    imgURL: `https://user-images.githubusercontent.com/51541006/61474004-33657400-a977-11e9-886f-b52cce97e8e2.png`,
+    alt: `sgt pepper george`
+    },
+    {
+    imgURL: `https://user-images.githubusercontent.com/51541006/61474005-33657400-a977-11e9-9801-7c057b5d327a.png`,
+    alt: `sgt pepper john`
+    },
+    {
+    imgURL: `https://user-images.githubusercontent.com/51541006/61474006-33657400-a977-11e9-8396-43afcee0efb4.png`,
+    alt: `sgt pepper paul`    
+    },
+    {
+    imgURL: `https://user-images.githubusercontent.com/51541006/61474009-35c7ce00-a977-11e9-9246-fe4d2960ecb6.png`,
+    alt: `sgt pepper ringo`
+    }
+];
---
+++ index.js
@@ -0,0 +1,118 @@
+let score = 0;
+let questionNumber = 0;
+
+function beginTheQuiz() {
+    $(`.quizStarter`).on('click', '.startButton', function(event) {
+        $(`.quizStarter`).remove();
+        $('.questionNumber').text(1);
+        renderNextQuestion();
+    });
+}
+
+function incrementQuestionNumber() {
+    questionNumber ++;
+}
+
+function incrementScore() {
+    score++;
+    $('.quizScore').text(score);
+}
+
+function renderNextQuestion() {
+    $(`.contentGoesHere`).html(generateQuestion());
+    $('.questionNumber').text(questionNumber + 1);
+    validateAnswer();    
+}
+
+function generateQuestion() {
+    if (questionNumber < questionsData.length) {
+    return `<div class="question"><h2>"${questionsData[questionNumber].question}"</h2></div>
+    <div class="answerList">
+    <form class="answers">
+        <fieldset>
+            <label class="answerOption" id="answerA">
+                <input type="radio" value="${questionsData[questionNumber].a}" name="answer" required><span>${questionsData[questionNumber].a}</span>
+            </label>
+            <label class="answerOption" id="answerB">
+                <input type="radio" value="${questionsData[questionNumber].b}" name="answer" required><span>${questionsData[questionNumber].b}</span>
+            </label>
+            <label class="answerOption" id="answerC">
+                <input type="radio" value="${questionsData[questionNumber].c}" name="answer" required><span>${questionsData[questionNumber].c}</span>
+            </label>
+            <label class="answerOption" id="answerD">
+                <input type="radio" value="${questionsData[questionNumber].d}" name="answer" required><span>${questionsData[questionNumber].d}</span>
+            </label>
+            <button type="submit" class="submitAnswer">Submit</button>
+        </fieldset>
+    </form>
+</div>`;
+}
+else {
+  renderResults();
+  $(`.score`).hide();
+}} 
+
+function validateAnswer() {
+    $('.answers').on('submit', function(event) {
+        event.preventDefault();
+        let selected = $('input:checked');
+        let userAnswer = selected.val();
+        let rightAnswer = `${questionsData[questionNumber].answer}`;
+        if (userAnswer === rightAnswer) {
+           youGotItRight();
+        }
+        else {
+            youGotItWrong();
+        }
+    });
+}
+
+function youGotItRight() {
+    let iconNum = Math.floor((Math.random() * 4) + 1);
+    $(`.contentGoesHere`).html(`<div class="feedbackMessage">
+    <img class="feedbackIcon" src="${iconData[iconNum].imgURL}" alt="${iconData[iconNum].alt}">
+    <h3>You got it right!</h3>
+</div>
+<button class="nextQuestionButton">Next Question</button>`);
+incrementScore();
+incrementQuestionNumber();
+nextQuestion();
+}
+
+function youGotItWrong() {
+    $(`.contentGoesHere`).html(`<div class="feedbackMessage">
+    <img class="feedbackIcon" src="${iconData[0].imgURL}" alt="${iconData[0].alt}">
+    <h3>You got it wrong!</h3>
+    <p>The correct answer is <span class="correctFeedbackAnswer"><strong>${questionsData[questionNumber].answer}</strong></span></p>
+</div>
+<button class="nextQuestionButton">Next Question</button>`);
+incrementQuestionNumber();
+nextQuestion();
+}
+
+function nextQuestion() {
+    $('main').on('click', '.nextQuestionButton', function(event) {
+        renderNextQuestion();
+    });
+}
+
+function renderResults() {
+    if (score >= 7) {
+   $(`.contentGoesHere`).html(`<div class="resultsMessage">
+   <h2>Congratulations!</h2>
+   <img class="resultsImage" src="https://user-images.githubusercontent.com/51541006/61474003-33657400-a977-11e9-8876-bb09b1230853.jpg" alt="beatles party">
+   <h3>You got ${score}/10 questions correct</h3>
+   <p>You are a true Beatles Superfan!</p>
+</div>
+<button class="restartQuiz" onclick="window.location.href = 'index.html';">Restart Quiz</button>`);
+} else {
+    $(`.contentGoesHere`).html(`<div class="resultsMessage">
+   <h2>Pretty good job!</h2>
+   <img class="resultsImage" src="https://user-images.githubusercontent.com/51541006/61474010-35c7ce00-a977-11e9-8876-dcfdfc05c5e2.jpg" alt="pointing beatles">
+   <h3>You got ${score}/10 questions correct</h3>
+   <p>Try again to see if you can do even better!</p>
+</div>
+<button class="restartQuiz" onclick="window.location.href = 'index.html';">Restart Quiz</button>`);
+}}
+
+$(beginTheQuiz);
